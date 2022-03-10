aa0d27020ee0616fb5a87dd94a0ef521b21dc5fe
elinamakarenko <68436291+elinamakarenko@users.noreply.github.com>
2022-03-10 17:20:25 +0200

    third-commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+galleryAdd(galleryItems);
+function galleryAdd (galleryArray) {
+    const markup = galleryArray.map(image=>
+        `<li><a class="gallery__item" href="${image.original}.jpg">
+        <img class="gallery__image" 
+        src="${image.preview}" 
+        alt="${image.description}" />
+      </a> </li>`).join('');
+      galleryRef.insertAdjacentHTML('beforeend', markup);
+}
 
-console.log(galleryItems);
+    let lightbox = new SimpleLightbox(".gallery a",   {
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+      });
+      lightbox.on('show.simplelightbox', function () {});
+
+
